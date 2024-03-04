93624c95ed5472efab834e5730205caa6090698e
rryan9 <67615024+rryan9@users.noreply.github.com>
2024-03-04 02:04:05 -0800

    Demo express app with ss auth

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var indexRouter = require("./routes/index.js");
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASEURL,
+    clientID: process.env.CLIENTID,
+    issuerBaseURL: process.env.ISSUER, 
+    clientSecret: process.env.CLIENTSECRET, 
+    //
+    
+    authorizationParams: {
+        response_type: 'code', 
+        audience: "http://localhost:5000",
+        scope: 'openid profile email'
+    }  
+  };
+
+var app = express();
+app.set('views','views');
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded(({extended:true})));
+app.use(express.static('public'));
+//auth
+app.use(auth(config));
+
+app.use('/', indexRouter);
+
+app.listen(3002, () => {
+    console.log('Express is running on port 3002');
+});
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+const {requiresAuth} = require("express-openid-connect");
+const axios = require('axios')
+
+router.get("/", (req, res) => {
+    console.log(req.oidc.isAuthenticated());
+    res.render("index", {
+        title: "Express Demo",
+        isAuthenticated: req.oidc.isAuthenticated(), 
+        user: req.oidc.user,
+    });
+});
+
+
+router.get("/secured", requiresAuth(), async (req, res) => {
+    let data = {}
+    //
+    const {token_type, access_token} = req.oidc.accessToken;
+
+    try {
+    const apiResponse = await axios.get('http://localhost:5000/private',
+    // need to pull this infomation when login, from the access token 
+    {
+        headers: {
+            authorization: `${token_type} ${access_token}`
+        }
+    })
+    
+    data = apiResponse.data
+    } catch(e) {}
+
+    res.render("secured", {
+        title: "Secure Page",
+        isAuthenticated: req.oidc.isAuthenticated(), 
+        user: req.oidc.user,
+        data
+    });
+});
+
+
+module.exports = router;
